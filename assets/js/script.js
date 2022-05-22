227846b77f5061260ff4c31871922048ee09910f
kaylab78 <kaylabackus78@gmail.com>
2022-05-22 14:02:48 -0500

    When the user clicks one of the answer options, a new question appears

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,12 @@ var timerEl = document.getElementById("timer");
 var mainEl = document.getElementById("main");
 var timeLeft = 75;
 var introText = document.getElementById("intro-text");
-var question = document.getElementById("question");
-var option = document.getElementById("option");
+var questionEl = document.getElementById("question");
+var optionEl = document.getElementById("option");
 
-// Question 1
-var questionOne = document.createElement("h2");
-
-var answerArea = document.createElement("ul");
+// Question and answer option elements
+var questionH2 = document.createElement("h2");
+var answerUl = document.createElement("ul");
 var li1 = document.createElement("li");
 var li2 = document.createElement("li");
 var li3 = document.createElement("li");
@@ -17,7 +16,7 @@ var li4 = document.createElement("li");
 
 // Click the start button. Timer starts. 
 function countdown() {
-    timerEl.textContent=timeLeft + " Seconds Remaining";
+    timerEl.textContent = timeLeft + " Seconds Remaining";
     if (timeLeft > 0) {
         timeLeft--
     } else {
@@ -26,61 +25,175 @@ function countdown() {
 }
 
 function timer() {
-    timerEl.textContent=timeLeft + " Seconds Remaining";
+    timerEl.textContent = timeLeft + " Seconds Remaining";
     setInterval(countdown, 1000);
-    startQuiz();
-}
-
-// First question appears. (Event Listeners)
-function startQuiz() {
     introText.setAttribute("style", "display:none");
     startEl.setAttribute("style", "display: none");
-
-    questionOne.textContent = "Which is NOT a commonly used JavaScript data type?";
-    question.appendChild(questionOne);
-
-    option.appendChild(answerArea);
-
-    li1.textContent = "String";
-    answerArea.appendChild(li1);
-
-    li2.textContent = "Boolean"
-    answerArea.appendChild(li2);
-
-    li3.textContent = "Alert"
-    answerArea.appendChild(li3);
-
-    li4.textContent = "Number"
-    answerArea.appendChild(li4);
+    firstQuestion();
 }
 
+// First question appears. Intro text and start button disappear.
 startEl.addEventListener("click", timer);
+function firstQuestion() {
+    questionH2.textContent = "Which is NOT a commonly used JavaScript data type?";
+    questionEl.appendChild(questionH2);
+
+    optionEl.appendChild(answerUl);
+
+    li1.textContent = "String";
+    answerUl.appendChild(li1);
+
+    li2.textContent = "Boolean";
+    answerUl.appendChild(li2);
+
+    li3.textContent = "Alert";
+    answerUl.appendChild(li3);
+
+    li4.textContent = "Number";
+    answerUl.appendChild(li4);
+
+    answerUl.addEventListener("click", secondQuestion);
+}
 
 
+// when the user clicks an answer in firstQuestion, the next question appears.
+function secondQuestion() {
+    questionH2.textContent = "The condition in an if/else statement is enclosed in which of the following?";
+    questionEl.appendChild(questionH2);
 
+    optionEl.appendChild(answerUl);
 
+    li1.textContent = "Quotation Marks";
+    answerUl.appendChild(li1);
 
-// var quizQuestions = [
-//     {
-//         question: "Which is NOT a commonly used JavaScript data type?",
-//         options: ["string", "boolean", "alert", "number"],
-//         answer: "alert"
-//     },
-//     {
-//         question: "Question 2",
-//         options: ["1", "2", "3"],
-//         answer: "2"
-//     }
-// ];
+    li2.textContent = "Curly Brackets";
+    answerUl.appendChild(li2);
 
+    li3.textContent = "Parentheses";
+    answerUl.appendChild(li3);
+
+    li4.textContent = "Square Brackets";
+    answerUl.appendChild(li4);
+
+    answerUl.addEventListener("click", thirdQuestion);
+}
+
+// when the user clicks an answer in secondQuestion, the next question appears.
+function thirdQuestion() {
+    questionH2.textContent = "Which of the following can be stored in arrays in JavaScript?";
+    questionEl.appendChild(questionH2);
+
+    optionEl.appendChild(answerUl);
+
+    li1.textContent = "Numbers & Strings";
+    answerUl.appendChild(li1);
+
+    li2.textContent = "Booleans";
+    answerUl.appendChild(li2);
+
+    li3.textContent = "Other Arrays";
+    answerUl.appendChild(li3);
+
+    li4.textContent = "All of the Above";
+    answerUl.appendChild(li4);
+
+    answerUl.addEventListener("click", fourthQuestion);
+}
+
+// when the user clicks an answer in thirdQuestion, the next question appears.
+function fourthQuestion() {
+    questionH2.textContent = "When being assigned to a variable, which of the following must string values be enclosed with?";
+    questionEl.appendChild(questionH2);
+
+    optionEl.appendChild(answerUl);
+
+    li1.textContent = "Commas";
+    answerUl.appendChild(li1);
+
+    li2.textContent = "Curly Brackets";
+    answerUl.appendChild(li2);
+
+    li3.textContent = "Quotation Marks";
+    answerUl.appendChild(li3);
+
+    li4.textContent = "Parentheses";
+    answerUl.appendChild(li4);
+}
 
 
 // Click one of the answer options. Another question appears. (Event Listeners)
-
-
 
 // When a question is answered incorrectly, time is subtracted from the clock. (Timers Intervals)
 
 // Quiz is over when all questions are answered or the timer reaches 0.
 
 // When the quiz is over, the user can save their score along with their initials. (Local storage)
+
+
+/*
+var quizQuestions = [
+    {
+        question: "Which is NOT a commonly used JavaScript data type?",
+        options: ["String", "Boolean", "Alert", "Number"],
+        answer: "Alert"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed in which of the following?",
+        options: ["Quotation Marks", "Curly Brackets", "Parentheses", "Square Brackets"],
+        answer: "Curly Brackets"
+    },
+    {
+        question: "Which of the following can be stored in arrays in JavaScript?",
+        options: ["Numbers & Strings", "Booleans", "Other Arrays", "All of the Above"],
+        answer: "All of the Above"
+    },
+    {
+        question: "When being assigned to a variable, which of the following must string values be enclosed with?",
+        options: ["Commas", "Curly Brackets", "Quotation Marks", "Parentheses"],
+        answer: "Quotation Marks"
+    }
+];
+
+function startQuiz() {
+    for (var i=0; i < 5; i++) {
+        questionEl.innerHTML = quizQuestions[i].question;
+        optionEl.innerHTML = "<li>" + quizQuestions[i].options[0] + "</li>" + "<li>" + quizQuestions[i].options[1] + "</li>" + "<li>" + quizQuestions[i].options[2] + "</li>" + "<li>" + quizQuestions[i].options[3] + "</li>";
+    }
+}
+*/
+
+
+/*
+var quizQuestions = [
+    {
+        question: "Which is NOT a commonly used JavaScript data type?",
+        options: ["String", "Boolean", "Alert", "Number"],
+        answer: "Alert"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed in which of the following?",
+        options: ["Quotation Marks", "Curly Brackets", "Parentheses", "Square Brackets"],
+        answer: "Curly Brackets"
+    },
+    {
+        question: "Which of the following can be stored in arrays in JavaScript?",
+        options: ["Numbers", "Strings", "Booleans", "Other Arrays", "All of the Above"],
+        answer: "All of the Above"
+    },
+    {
+        question: "When being assigned to a variable, which of the following must string values be enclosed with?",
+        options: ["Commas", "Curly Brackets", "Quotation Marks", "Parentheses"],
+        answer: "Quotation Marks"
+    }
+];
+console.log(quizQuestions[0]); 
+
+function startQuiz() {
+    for (var i=0; i < 5; i++) {
+        mainEl.appendChild(quizQuestions[i]);
+    }
+}
+*/
+// optionEl.addEventListener("click", function());
+
+// innerHTML
