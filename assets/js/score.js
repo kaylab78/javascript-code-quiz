49fcc7cf4c98124cef9852041e94cce22bbb8d5f
kaylab78 <kaylabackus78@gmail.com>
2022-05-25 09:10:21 -0500

    Get elements from highscores.html

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,3 +1,6 @@
 // print scores (getting something from local storage, JSON.parse)
 // sort scores (.sort)
 // clear scores (localStorage.remove(highscores))
+
+var scoreListEl = document.getElementById("scoreList");
+var clearEl = document.getElementById("clear");
